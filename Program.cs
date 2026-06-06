c7a6e4704312646dc07a71c2d60f97b6ff073fee
krevetkqqq <antosinoftenksvov@gmail.com>
2026-06-06 18:37:23 +0300

    .

--- Program.cs
+++ Program.cs
@@ -5,13 +5,10 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using JsonBeautifier;
 
-// Регистрируем провайдера кодировок для поддержки CP866
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-// Настройка UTF-8 для вывода (чтобы видеть кириллицу в консоли)
 Console.OutputEncoding = Encoding.UTF8;
 
-// Устанавливаем кодировку ввода CP866 (русская консоль Windows)
 Console.InputEncoding = Encoding.GetEncoding(866);
 
 using RSA rsa = RSA.Create();
@@ -19,8 +16,12 @@ using RSA rsa = RSA.Create();
 var httpClient = new HttpClient();
 httpClient.BaseAddress = new Uri("http://localhost:5000");
 var token = string.Empty;
+string? currentChat = null;
+Timer? pollingTimer = null;
+string? lastMessagesResponse = null;
 Console.WriteLine("Enter host or whitespace to http://localhost:5000");
 var input = Console.ReadLine();
+
 if (!string.IsNullOrWhiteSpace(input))
 {
     httpClient.BaseAddress = new Uri(input);
@@ -34,6 +35,98 @@ if (!File.Exists("rsa.env"))
 var rsaKeyPem = File.ReadAllText("rsa.env");
 var rsaKeyBytes = Convert.FromBase64String(rsaKeyPem);
 rsa.ImportRSAPublicKey(rsaKeyBytes, out _);
+async void PollChat(object? state)
+{
+    if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(currentChat))
+        return;
+
+    try
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, "chat")
+        {
+            Content = JsonContent.Create(new { chat = currentChat })
+        };
+        var response = await httpClient.SendAsync(request);
+        var answer = await response.Content.ReadAsStringAsync();
+
+
+        if (lastMessagesResponse == null)
+        {
+            lastMessagesResponse = answer;
+            return;
+        }
+
+        if (HaveNewMessages(lastMessagesResponse, answer))
+        {
+            Console.WriteLine($"\n[Новые сообщения в чате '{currentChat}']:");
+            PrintNewMessages(lastMessagesResponse, answer);
+            lastMessagesResponse = answer;
+            Console.Write("\nПожалуйста, введите команду + параметры: ");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"\nОшибка опроса: {ex.Message}");
+    }
+}
+
+static bool HaveNewMessages(string oldResponse, string newResponse)
+{
+    try
+    {
+        var oldMessages = ExtractMessagesArray(oldResponse);
+        var newMessages = ExtractMessagesArray(newResponse);
+
+        if (oldMessages == null || newMessages == null)
+            return oldResponse != newResponse;
+
+        return newMessages.Count > oldMessages.Count;
+    }
+    catch
+    {
+        return oldResponse != newResponse;
+    }
+}
+
+static JsonArray? ExtractMessagesArray(string jsonResponse)
+{
+    var node = JsonNode.Parse(jsonResponse);
+    var message = node?["message"];
+    return message?["messages"]?.AsArray();
+}
+
+void PrintNewMessages(string oldResponse, string newResponse)
+{
+    try
+    {
+        var oldMessages = ExtractMessagesArray(oldResponse);
+        var newMessages = ExtractMessagesArray(newResponse);
+
+        if (oldMessages == null || newMessages == null)
+        {
+            JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(newResponse)!);
+            return;
+        }
+        var onlyNew = newMessages.Skip(oldMessages.Count);
+        foreach (var msg in onlyNew)
+        {
+            Console.WriteLine($"  {msg}");
+        }
+    }
+    catch
+    {
+        JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(newResponse)!);
+    }
+}
+
+void StartPolling()
+{
+    if (pollingTimer == null && !string.IsNullOrEmpty(token))
+    {
+        pollingTimer = new Timer(PollChat, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+        Console.WriteLine("Фоновый опрос чата запущен (каждые 5 секунд).");
+    }
+}
 while (true)
 {
     Console.WriteLine("Please enter command + params");
@@ -51,6 +144,7 @@ while (true)
         var answer = await response.Content.ReadAsStringAsync();
          JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(answer)!);
         token = JsonNode.Parse(answer)["token"]!.GetValue<string>();
+        StartPolling();
     }
     else if (commandInput.StartsWith("login"))
     {
@@ -63,6 +157,7 @@ while (true)
         var answer = await response.Content.ReadAsStringAsync();
          JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(answer)!);
         token = JsonNode.Parse(answer)["token"]!.GetValue<string>();
+        StartPolling();
     }
     else if (commandInput.StartsWith("token"))
     {
@@ -94,18 +189,24 @@ while (true)
         });
         var answer = await response.Content.ReadAsStringAsync();
          JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(answer)!);
+
+        currentChat = commandInput.Split(' ')[1];
+        lastMessagesResponse = null;
+        Console.WriteLine($"Теперь вы в чате '{currentChat}'");
     }
     else if (commandInput.StartsWith("chat"))
     {
         var request = new HttpRequestMessage(HttpMethod.Get, "chat")
         {
             Content = JsonContent.Create(new {
-                chat = commandInput.Split(' ')[1]
+                chat = currentChat ?? commandInput.Split(' ')[1] 
             })
         };
         var response = await httpClient.SendAsync(request);
         var answer = await response.Content.ReadAsStringAsync();
          JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(answer)!);
+
+        lastMessagesResponse = answer;
     }
     else if (commandInput.StartsWith("leave"))
     {
@@ -115,6 +216,8 @@ while (true)
         });
         var answer = await response.Content.ReadAsStringAsync();
          JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(answer)!);
+         currentChat = null;
+        Console.WriteLine("Активный чат сброшен");
     }
     else if (commandInput.StartsWith("deletechat"))
     {
@@ -137,18 +240,54 @@ while (true)
         });
         var answer = await response.Content.ReadAsStringAsync();
          JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(answer)!);
+        currentChat = commandInput.Split(' ')[1];
+        lastMessagesResponse = null;
+        Console.WriteLine($"Теперь вы в чате '{currentChat}'");
     }
     else if (commandInput.StartsWith("message"))
     {
-        var encryptedBytes = rsa.Encrypt(Encoding.UTF8.GetBytes(commandInput.Split(' ')[2]), RSAEncryptionPadding.OaepSHA256);
-        var encryptedBase64 = Convert.ToBase64String(encryptedBytes);
-        var response = await httpClient.PostAsJsonAsync("message", new
+        if (commandInput.Split(' ').Length < 2 )
         {
-            token = token,
-            chat = commandInput.Split(' ')[1],
-            message = encryptedBase64
-        });
+            Console.WriteLine("Недостаточно параметров для команды 'message'. Формат: message <chat> <text>");
+            continue;
+        }
+        HttpResponseMessage response;
+        if (commandInput.Split(' ').Length == 2 && currentChat != null)
+        {
+            var encryptedBytes = rsa.Encrypt(Encoding.UTF8.GetBytes(commandInput.Split(' ')[1]), RSAEncryptionPadding.OaepSHA256);
+            var encryptedBase64 = Convert.ToBase64String(encryptedBytes);
+            response = await httpClient.PostAsJsonAsync("message", new
+            {
+                token = token,
+                chat = currentChat,
+                message = encryptedBase64
+            });
+        }
+        else
+        {
+            var encryptedBytes = rsa.Encrypt(Encoding.UTF8.GetBytes(commandInput.Split(' ')[1]), RSAEncryptionPadding.OaepSHA256);
+            var encryptedBase64 = Convert.ToBase64String(encryptedBytes);
+            response = await httpClient.PostAsJsonAsync("message", new
+            {
+                token = token,
+                chat = commandInput.Split(' ')[1],
+                message = encryptedBase64
+            });
+            currentChat = commandInput.Split(' ')[2];
+            Console.WriteLine($"Теперь вы в чате '{currentChat}'");
+        }
         var answer = await response.Content.ReadAsStringAsync();
          JsonBeautifier.JsonBeautifier.FromBeauty(JsonBeautifier.JsonBeautifier.PrettyJson(answer)!);
+    }
+    else if (commandInput.StartsWith("activechat"))
+    {
+        Console.WriteLine(currentChat ?? "Активный чат не выбран");
+    }
+    else if (commandInput.StartsWith("setchat"))
+    {
+        var chatName = commandInput.Split(' ')[1];
+        currentChat = chatName;
+        lastMessagesResponse = null;
+        Console.WriteLine($"Активный чат установлен на '{chatName}'");
     }
 }
